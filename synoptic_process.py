eb70a0d659a8128d76e20e57cb7a08e956354c0b
Arthur.Cascardo <artnc2010@gmail.com>
2022-06-30 15:40:13 -0300

    Synced threads and working synoptic

--- synoptic_process.py
+++ synoptic_process.py
@@ -6,9 +6,10 @@ PORT = 52415  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    data = input()
     while True:
-        s.sendall(bytes(json.dumps(data).encode()))
-        data_rcv = json.loads(s.recv(1024))
-        print(f"{data_rcv}")
+        data = input()
+        for i in range(100):
+            s.sendall(bytes(json.dumps(data).encode()))
+            data_rcv = json.loads(s.recv(1024))
+            print(f"{data_rcv}")
 
--- threads.py
+++ threads.py
@@ -15,15 +15,12 @@ h_0 = 1
 qin_0 = 1
 period = 0.05
 t = 0
-qin = 0
+qin = [1,1]
 
 
 logging.info("Thread %s: starting")
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 52415  # Port to listen on (non-privileged ports are > 1023
-
-
-
 
 
 def softPLC_thread():
@@ -34,15 +31,21 @@ def softPLC_thread():
     conn, addr = s.accept()
     while True:
         data = conn.recv(1024)
-        href_aux.append(float(json.loads(data)))
+        if not lock.locked():
+            lock.acquire()  #Entra na seção critica
+            href_aux.append(float(json.loads(data)))
+            lock.release()  #Sai da seção critica
         if not data:
             logging.info("Thread %s: finishing")
         if ht:
-            pid = PID(1, 3, 0.05, setpoint=href_aux[-1])
-            pid.output_limits = (0, None)
-            qin = pid(ht[-1])
-            h_0 = ht[-1]
-            conn.sendall(json.dumps(f"Vazão de entrada: {qin}\n"
+            if not lock.locked():
+                lock.acquire()
+                pid = PID(2, 6, 0.05, setpoint=href_aux[-1])
+                pid.output_limits = (0, None)
+                qin[-1] = pid(ht[-1])
+                h_0 = ht[-1]
+                lock.release()
+            conn.sendall(json.dumps(f"Vazão de entrada: {qin[-1]}\n"
                                     f"Vazão de saida: {qout[-1]}\n"
                                     f"Altura referencia: {href_aux[-1]}\n"
                                     f"Altura atual: {ht[-1]}").encode())
@@ -52,18 +55,22 @@ def softPLC_thread():
             time.sleep(2 * period)
 
 
-
 def process_thread():
     res = 0
     while True:
-        if href_aux:
-            qin = res
-            res = rk.rk4(h_0, qin, ht[-1], 2) # Calcula pelo metodo Runge-Kutta
+        if href_aux and not lock.locked():
+            lock.acquire()
+            qin.append(res)
+            res = rk.rk4(qin[-2], ht[-1], qin[-1], 2)  # Calcula pelo metodo Runge-Kutta
+            qin.append(res)
             href_list.append(href_aux[-1])  # Acrescenta referencia na lista
             ht.append(res)
             qout.append(0.5 * math.sqrt(ht[-1]))  # Calcula e salva vazão em função da altura h(t)
+            lock.release()
             time.sleep(period)
 
+
+lock = threading.Lock()
 
 # process_thread
 logging.info("Main    : create and start thread %d.", 1)
